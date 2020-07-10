3ded4aa7ed9c672d34da39685cbe2098cf596ebf
gwanwoodev <gwanwoodev@kakao.com>
2020-07-10 11:14:22 +0000

    cors update

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { Routes } from "./routes/router";
 
 class App {
@@ -10,6 +11,7 @@ class App {
         this.app = express();
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
+		this.app.use(cors());
         this.routePrv.routes(this.app);
         this.app.listen(this.SERVER_PORT, () => console.log(`listen port ${this.SERVER_PORT}`));
     }
