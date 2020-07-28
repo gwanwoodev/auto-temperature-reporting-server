973450850a249a2237506d921c739fe069ecf260
unknown <jm.moon@yonsei.ac.kr>
2020-07-28 19:37:18 +0900

    update

--- routes/router.ts
+++ routes/router.ts
@@ -1,32 +1,37 @@
 import { Request, Response } from "express";
 import MainController from "../controllers/main.controller";
 export class Routes {
-    public routes(app): void {
-        app.route("/api/report").post(async (req: Request, res: Response) => {
-            let token: any = await MainController.login({ idVal: req.body.idVal, pwVal: req.body.pwVal, hugaDate: req.body.hugaDate, schedule: true });
-            // let result = await MainController.report(token);
-            res.json({ token });
-        })
+  public routes(app): void {
+    app.route("/api/report").post(async (req: Request, res: Response) => {
+      let token: any = await MainController.login({
+        idVal: req.body.idVal,
+        pwVal: req.body.pwVal,
+        hugaDate: req.body.hugaDate,
+        schedule: true,
+      });
+      // let result = await MainController.report(token);
+      res.json({ token });
+    });
 
-        app.route("/api/test").get((req: Request, res: Response) => {
-            res.json(globalThis.slaveList);
+    app.route("/api/test").get((req: Request, res: Response) => {
+      res.json(globalThis.slaveList);
+    });
+
+    app.route("/api/queue").get((req: Request, res: Response) => {
+      let tempSlaveList = [];
+      if (globalThis.slaveList) {
+        tempSlaveList = globalThis.slaveList.filter((slave) => {
+          const today = new Date();
+          today.setHours(0, 0, 0, 0);
+          const slaveDate = new Date(slave.hugaDate);
+          slaveDate.setDate(slaveDate.getDate() - 3);
+          slaveDate.setHours(0, 0, 0, 0);
+          const endDate = new Date(slave.hugaDate);
+          endDate.setHours(0, 0, 0, 0);
+          return today >= slaveDate && endDate >= today;
         });
-		
-		app.route("/api/queue").get((req: Request, res: Response) => {
-			let tempSlaveList = [];
-			if(globalThis.slaveList) {
-			  tempSlaveList = globalThis.slaveList.filter((slave) => {
-				const today = new Date();
-				today.setHours(0, 0, 0, 0);
-				const slaveDate = new Date(slave.hugaDate);
-				slaveDate.setDate(slaveDate.getDate() - 2);
-				slaveDate.setHours(0, 0, 0, 0);
-				const endDate = new Date(slave.hugaDate);
-				endDate.setHours(0, 0, 0, 0);
-				return today >= slaveDate && endDate >= today;
-			  });
-			res.json(tempSlaveList);
-			}
-		});
-    }
+        res.json(tempSlaveList);
+      }
+    });
+  }
 }
